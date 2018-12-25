a32f01591869f73a49ee225a14468f4efac2e799
calmosowl <calmol.owl@gmail.com>
2018-12-25 22:31:31 +0200

    feat: Added markdown feature.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const 
   gulp =  require('gulp'),
   rigger = require('gulp-rigger'),
+  rename = require("gulp-rename"),
   sass = require('gulp-sass'),
   prefixer = require('gulp-autoprefixer'),
   watch = require('gulp-watch'),
-  browserSync = require("browser-sync"),
+  browserSync = require('browser-sync'),
   markdown = require('gulp-markdown'),
   reload = browserSync.reload;
 
@@ -14,18 +15,19 @@ const path = {
     js: './js/',
     css: 'css/',
     img: './images/',
+    markdown: './html/',
     fonts: './fonts/'
   },
   src: {
-    markdown: 'src/**/*.md',
-    html: 'src/*.html',
+    markdown: 'src/markdown/*.md',
+    html: './html/*.html',
     style: 'src/sass/main.scss',
     img: 'src/images/**/*.*',
     fonts: 'src/fonts/**/*.*'
   },
   watch: {
-    markdown: 'src/**/*.md',
-    html: 'src/**/*.html',
+    markdown: 'src/markdown/*.md',
+    html: './html/*.html',
     js: 'src/js/**/*.js',
     style: 'src/sass/**/*.scss',
     img: 'src/images/**/*.*',
@@ -43,15 +45,17 @@ const config = {
   logPrefix: "calmos"
 };
 
-gulp.task('markdown', () =>
-  gulp.src(path.src.markdown)
-    .pipe(markdown())
-    .pipe(gulp.dest('./'))
+gulp.task('markhtml', () =>
+gulp.src(path.src.markdown)
+  .pipe(markdown())
+  .pipe(rename(function (path) {
+    path.extname = ".html";
+  }))
+  .pipe(gulp.dest('html'))
 );
 
 gulp.task('html', function () {
   gulp.src(path.src.html)
-    .pipe(markdown())
     .pipe(rigger())
     .pipe(gulp.dest(path.build.html))
     .pipe(reload({ stream: true }));
@@ -72,9 +76,14 @@ gulp.task('webserver', function () {
 });
 
 gulp.task('watch', function () {
-  watch([path.watch.html], function (event, cb) {
-    gulp.start('html'); 
+  watch([path.watch.markdown], function (event, cb) {
+    gulp.start('markhtml'); 
   });
+  // Необходимо перенести склейку html частей в один файл в папку src, а
+  // результат отдавать в ./index.html
+  // watch([path.watch.html], function (event, cb) {
+  //   gulp.start('html');
+  // });
   watch([path.watch.style], function (event, cb) {
     gulp.start('style'); 
   });
---
+++ src/markdown/experience.md
@@ -0,0 +1,55 @@
+### Work Experience
+
+##### Enestech
+
+##### Smile Expo
+
+<p class="date">09.2016 – Present</p>
+
+Applied Programmer
+
+*   front-end: Developing web components, widgets(js, html, css). Write html web pages. Optimize website performance
+*   back-end: Build a site using Yii
+
+##### Ermilove marketing
+
+<p class="date">09.2015 – 09.2016</p>
+
+Frontend developer
+
+*   Converted Photoshop layouts to web pages using HTML, CSS, and JavaScript
+*   Provide assistance to the beck-end developers in troubleshooting and coding
+*   Development of web interfaces (corporate sites, online stores, landing-page)
+*   Updating existing solutions, fixing errors
+
+##### JSC 'BANK 'FINANCE AND CREDIT'
+
+<p class="date">03.2008 – 08.2015</p>
+
+Head of Corporate Banking Credit Department
+
+<p class="date">03.2007 – 03.2008</p>
+
+Head of corporate banking credit administration sector
+
+<p class="date">04.2006 – 04.2007</p>
+
+Department of credit administration. Senior economist.
+
+##### JSC Privatbank
+
+<p class="date">10.2005 – 04.2006</p>
+
+Expert on monitoring loans of legal entities.
+
+##### LLC Machine-Building Plant 100
+
+<p class="date">02.2005 – 04.2005</p>
+
+Metrology engineer
+
+##### LLC Scientific-Production Enterprise "Luganskemal"
+
+<p class="date">08.2003 – 06.2004</p>
+
+Head of the Laboratory of Metrology
